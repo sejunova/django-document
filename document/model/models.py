fb76d8e9f1043be5103d3aaf771fe34c574ba4a6
Sejun Im <sejun425@gmail.com>
2017-10-10 14:07:47 +0900

    manytoone

--- document/model/models.py
+++ document/model/models.py
@@ -9,3 +9,30 @@ class Person(models.Model):
     )
     name = models.CharField(max_length=60)
     shirt_size = models.CharField(max_length=1, choices=SHIRT_SIZES)
+
+class Fruit(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+
+class Manufacturer(models.Model):
+    company_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.company_name
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.manufacturer.name} - {self.car_name}'
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    teacher = models.ForeignKey('self', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Topping(models.Model):
+    name = models.CharField(max_length=30)
