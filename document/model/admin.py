f2a7d676951dec94ed08ba4c6101202a39408fda
Sejun Im <sejun425@gmail.com>
2017-10-10 16:52:12 +0900

    idol

--- document/model/admin.py
+++ document/model/admin.py
@@ -1,9 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Pizza, Topping, FacebookUser, InstagramUser
+from .models import Pizza, Topping, FacebookUser, InstagramUser, Idol, Group, Membership
 
-admin.site.register(Pizza)
-admin.site.register(Topping)
-admin.site.register(FacebookUser)
-admin.site.register(InstagramUser)
+admin.site.register(Idol)
+admin.site.register(Group)
+admin.site.register(Membership)
